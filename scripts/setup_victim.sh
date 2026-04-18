9b957f6931ce1d4c974384984949892a8ef8373a
Evaldas Dmitrišin <81704689+32Butterfly@users.noreply.github.com>
2026-04-18 21:00:07 +0300

    Fix whitespaces/tabs otherwise script cant run normally

--- scripts/setup_victim.sh
+++ scripts/setup_victim.sh
@@ -19,12 +19,12 @@ if [ "$response" == "y" ] ; then
 
 	# Add the repository to Apt sources:
 	tee /etc/apt/sources.list.d/docker.sources <<EOF
-	Types: deb
-	URIs: https://download.docker.com/linux/debian
-	Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
-	Components: stable
-	Architectures: $(dpkg --print-architecture)
-	Signed-By: /etc/apt/keyrings/docker.asc
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
 	apt update
