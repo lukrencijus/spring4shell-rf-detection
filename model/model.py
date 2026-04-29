10500d0c2831fa56fe24d7ca6354393a4ddf0ad8
Karolis Zabulis <karoliszabulis@gmail.com>
2026-04-29 16:18:44 +0300

    Rewritten valid traffic script to use python requests.

--- model/model.py
+++ model/model.py
@@ -5,13 +5,17 @@ from sklearn.utils import shuffle
 df = pd.read_csv('../dataset.csv')
 
 # Duration is maybe unnecessary as the delays that were simulated don't differentiate that much
-required_fields = [ 'label', 'bidirectional_duration_ms', 'bidirectional_packets', 'bidirectional_bytes', 'src2dst_duration_ms', 
-                    'src2dst_packets', 'src2dst_bytes', 'dst2src_duration_ms', 'dst2src_packets', 'dst2src_bytes', 'bidirectional_syn_packets',
+required_fields = [ 'label', 'bidirectional_packets', 
+                    'src2dst_packets',  'dst2src_packets', 'bidirectional_syn_packets',
                     'bidirectional_ack_packets', 'bidirectional_psh_packets', 'src2dst_syn_packets', 'src2dst_ack_packets', 'src2dst_psh_packets',
-                    'dst2src_syn_packets', 'dst2src_ack_packets', 'dst2src_psh_packets', 'dst2src_max_ps', 'src2dst_max_ps',
-                    'dst2src_mean_ps', 'dst2src_rst_packets', 'src2dst_rst_packets' ]
+                    'dst2src_syn_packets', 'dst2src_ack_packets', 'dst2src_psh_packets', 
+                     'dst2src_rst_packets', 'src2dst_rst_packets' ]
 
-# 'dst2src_min_ps' 'src2dst_min_ps' 'src2dst_mean_ps'
+# Duration variables, might be compromised due to difference of tools
+# 'bidirectional_duration_ms', 'src2dst_duration_ms', 'dst2src_duration_ms', 
+
+# Removed due to size fingerprinting caused by different tools
+# 'dst2src_min_ps' 'src2dst_min_ps' 'src2dst_mean_ps' 'src2dst_max_ps', 'dst2src_max_ps', 'dst2src_mean_ps', 'src2dst_bytes', 'dst2src_bytes', 'bidirectional_bytes',
 
 # Split dataframes according to their purpose
 df_learning = df[df['split'] == 'learning']
@@ -31,13 +35,12 @@ y_test = df_testing.iloc[:, 0]
 rf = RandomForestClassifier()
 rf.fit(X_train, y_train)
 
-importances = pd.Series(
+y_pred = rf.predict(X_test)
+print(rf.score(X_test, y_test))
+
+features = pd.Series(
     rf.feature_importances_,
     index=X_train.columns
 ).sort_values(ascending=False)
-
-print(importances)
-
-y_pred = rf.predict(X_test)
-print(rf.score(X_test, y_test))
+print(features)
 
---
+++ scripts/simulate_valid_traffic.py
@@ -0,0 +1,113 @@
+import requests
+import random
+import sys
+from time import sleep
+
+random_constant = 5000000
+base="http://localhost:8080"
+
+def walk_through_courses(i):
+    page = i / 5
+    requests.get(base+"/")
+    requests.post(base+"/save", data = {
+        "courseName": f"NewCourse{i}",
+        "instructor": f"CourseInstructor{i}",
+        "email": f"CourseEmail{i}"
+    })
+    requests.get(base+"/?sortField=courseName&sortDir=asc")
+    requests.get(base+"/add")
+    requests.post(base+"/save", data={
+        "id": i,
+        "courseName": f"NewCourse{random.randrange(1, random_constant)}",
+        "instructor": f"CourseInstructor{random.randrange(1, random_constant)}",
+        "email": f"CourseEmail{random.randrange(1, random_constant)}"
+    })
+    requests.get(base+f"/page/{page}?sortField=courseName&sortDir=desc")
+    requests.delete(base+f"/delete/+{i}")
+
+def walk_through_sutents(i):
+    requests.post(base+"/students/save", data={
+        "id": i,
+        "fisrtName": f"RandomName{random.randrange(1, random_constant)}",
+        "lastName": f"RandomLastName{random.randrange(1, random_constant)}",
+        "studentId": f"S11666684486{random.randrange(1, random_constant)}",
+        "major": f"SomeVeryInterestingMajor{random.randrange(1, random_constant)}"
+    })
+    requests.get(base+"/students/")
+    requests.get(base+f"/students/update/{i}")
+    requests.post(base+"/students/save", data={
+        "fisrtName": f"RandomName{random.randrange(1, random_constant)}",
+        "lastName": f"RandomLastName{random.randrange(1, random_constant)}",
+        "studentId": f"S11666684486{random.randrange(1, random_constant)}",
+        "major": f"SomeVeryInterestingMajor{random.randrange(1, random_constant)}"
+    })
+    delete=int(i/10)
+    requests.delete(base+f"/students/delete/{delete}")
+
+def walk_through_instructors(i):
+    requests.post(base+"/instructors/save", data={
+        "id": i,
+        "instructorName": f"VeryNiceName{random.randrange(1, random_constant)}",
+        "employeeId": f"S84894{random.randrange(1, random_constant)}",
+        "email": f"{random.randrange(1, random_constant)}@mail.co",
+        "officeLocation": f"OfficeAt{random.randrange(1, random_constant)}"
+    })
+    requests.get(base+"/instructors/")
+    requests.get(base+f"/instructors/update{i}")
+    requests.post(base+"/instructors/save", data={
+        "instructorName": f"VeryNiceName{random.randrange(1, random_constant)}",
+        "employeeId": f"S84894{random.randrange(1, random_constant)}",
+        "email": f"{random.randrange(1, random_constant)}@mail.co",
+        "officeLocation": f"OfficeAt{random.randrange(1, random_constant)}"
+    })
+    delete=int(i/10)
+    requests.delete(base+f"/instructors/delete/{i}")
+
+def walk_through_departments(i):
+    requests.post(base+"/departments/save", data={
+        "id": i,
+        "departmentName": f"VeryBuilding{random.randrange(1, random_constant)}",
+        "building": f"BuildingAtS84894{random.randrange(1, random_constant)}",
+        "email": f"{random.randrange(1, random_constant)}@mail.co",
+        "headOfDepartment": f"DepartmentOwner{random.randrange(1, random_constant)}"
+    })
+    requests.get(base+"/departments/")
+    requests.get(base+f"/departments/update{i}")
+    requests.post(base+"/departments/save", data={
+        "departmentName": f"VeryBuilding{random.randrange(1, random_constant)}",
+        "building": f"BuildingAtS84894{random.randrange(1, random_constant)}",
+        "email": f"{random.randrange(1, random_constant)}@mail.co",
+        "headOfDepartment": f"DepartmentOwner{random.randrange(1, random_constant)}"
+    })
+    delete=int(i/10)
+    requests.delete(base+f"/departments/delete/{delete}")
+
+def invalid_traffic():
+    requests.get(base+"/root/incorrect")
+    requests.post(base+"/more/incorrect")
+    requests.put(base+"/where/am/i")
+    requests.delete(base+"/very/interesting")
+
+if len(sys.argv) != 2 or sys.argv[1].isdigit() is False:
+    print("Program expects iteration amount to be passed as an argument.")
+    print("Usage: python3 simulate_valid_traffic.py <number_of_iterations>")
+    sys.exit(0)
+else:
+    if int(sys.argv[1]) > 0:
+        rounds = int(sys.argv[1])
+    else:
+        print("Negative iterration amount, defaulting to 1")
+        rounds = 1
+try:
+    for i in range(rounds):
+        print(f"Loop: {i+1}/{rounds}")
+        walk_through_courses(i)
+        walk_through_departments(i)
+        invalid_traffic()
+        walk_through_instructors(i)
+        walk_through_sutents(i)
+        invalid_traffic()
+        sleep(0.2)
+except Exception as e:
+    print("Something went wrong")
+    raise e
